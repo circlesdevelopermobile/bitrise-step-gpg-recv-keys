38947dd06359e690f95cab3c67017dd912963df5
Viel Asis <its.viel@gmail.com>
2021-12-09 13:51:09 +0800

    typo in cut

--- step.sh
+++ step.sh
@@ -4,7 +4,7 @@ echo "receiving keys from: ${gpg_keyserver_url}"
 
 IFS=$'\n'
 for i in $trusted_pk_signatures; do
-    [[ $i = \#* ]] && echo "INFO: \"$(echo $i | cut -d'# +' -f 2)\"" && continue
+    [[ $i = \#* ]] && echo "INFO: \"$(echo $i | cut -d'#' -f 2)\"" && continue
     gpg --keyserver ${gpg_keyserver_url} --recv-keys $i
 done
 exit 0
